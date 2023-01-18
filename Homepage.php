699669fb0ebcc02b551b3851aa19cb4c41ef91e1
Filip Jansson <fredrikfilip.fj@gmail.com>
2023-01-18 19:59:10 +0100

    Add Homepage

---
+++ Homepage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="Status">
+        <h1 style="float: left; margin-right: 2rem;">S</h1>
+        <h1 style="float: left; margin-right: 2rem;">T</h1>
+        <h1 style="float: left; margin-right: 2rem;">A</h1>
+        <h1 style="float: left; margin-right: 2rem;">T</h1>
+        <h1 style="float: left; margin-right: 2rem;">U</h1>
+        <h1 style="float: left; margin-right: 2rem;">S</h1>
+    </div>
+    <div class="MyWork">
+        <h1 style="float: left; margin-right: 2rem;">M</h1>
+        <h1 style="float: left; margin-right: 2rem;">Y</h1>
+        <h1 style="float: left; margin-right: 2rem;"> </h1>
+        <h1 style="float: left; margin-right: 2rem;">W</h1>
+        <h1 style="float: left; margin-right: 2rem;">O</h1>
+        <h1 style="float: left; margin-right: 2rem;">R</h1>
+        <h1 style="float: left; margin-right: 2rem;">K</h1>
+    </div>
+    <div class="GitHub">
+        <h1 style="float: left; margin-right: 2rem;">G</h1>
+        <h1 style="float: left; margin-right: 2rem;">I</h1>
+        <h1 style="float: left; margin-right: 2rem;">T</h1>
+        <h1 style="float: left; margin-right: 2rem;">H</h1>
+        <h1 style="float: left; margin-right: 2rem;">U</h1>
+        <h1 style="float: left; margin-right: 2rem;">B</h1> 
+    </div>
+</body>
+</html>
