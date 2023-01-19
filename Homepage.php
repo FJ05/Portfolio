bc80533c1bd7cd2c6875730107a080b9bb8ef22d
FJ05 <fredrikfilip.fj@gmail.com>
2023-01-19 15:31:32 +0100

    Add animated text for the main menu

--- Homepage.php
+++ Homepage.php
@@ -6,32 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/stylesAnimation.css">
+    <script src="js/GraphicalScripts.js"></script>
 </head>
 <body>
-    <div class="Status">
-        <h1 style="float: left; margin-right: 2rem;">S</h1>
-        <h1 style="float: left; margin-right: 2rem;">T</h1>
-        <h1 style="float: left; margin-right: 2rem;">A</h1>
-        <h1 style="float: left; margin-right: 2rem;">T</h1>
-        <h1 style="float: left; margin-right: 2rem;">U</h1>
-        <h1 style="float: left; margin-right: 2rem;">S</h1>
-    </div>
-    <div class="MyWork">
-        <h1 style="float: left; margin-right: 2rem;">M</h1>
-        <h1 style="float: left; margin-right: 2rem;">Y</h1>
-        <h1 style="float: left; margin-right: 2rem;"> </h1>
-        <h1 style="float: left; margin-right: 2rem;">W</h1>
-        <h1 style="float: left; margin-right: 2rem;">O</h1>
-        <h1 style="float: left; margin-right: 2rem;">R</h1>
-        <h1 style="float: left; margin-right: 2rem;">K</h1>
-    </div>
-    <div class="GitHub">
-        <h1 style="float: left; margin-right: 2rem;">G</h1>
-        <h1 style="float: left; margin-right: 2rem;">I</h1>
-        <h1 style="float: left; margin-right: 2rem;">T</h1>
-        <h1 style="float: left; margin-right: 2rem;">H</h1>
-        <h1 style="float: left; margin-right: 2rem;">U</h1>
-        <h1 style="float: left; margin-right: 2rem;">B</h1> 
+    <div id="text";>
+        <div class="HighLigt" id="Status">
+            <h1 style="float: left; margin-right: 2rem;" id="St-1">S</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="St-2">T</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="St-3">A</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="St-4">T</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="St-5">U</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="St-6">S</h1>
+        </div>
+        <div class="HighLigt" id="MyWork">
+            <h1 style="float: left; margin-right: 2rem;" id="My-1">M</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="My-2">Y</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="My-3"> </h1>
+            <h1 style="float: left; margin-right: 2rem;" id="My-4">W</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="My-5">O</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="My-6">R</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="My-7">K</h1>
+        </div>
+        <div class="HighLigt" id="GitHub">
+            <h1 style="float: left; margin-right: 2rem;" id="Git-1">G</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="Git-2">I</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="Git-4">T</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="Git-5">H</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="Git-6">U</h1>
+            <h1 style="float: left; margin-right: 2rem;" id="Git-7">B</h1> 
+        </div>
     </div>
 </body>
 </html>
